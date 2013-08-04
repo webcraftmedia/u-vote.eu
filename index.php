55d0bde13c9f3b0839957ef378174cdc0bae8bbf
rylon <rylon@web.de>
2013-08-04 23:34:01 +0200

    fixed uvote includes,fixed webcraft new api, fixed redestoff new api

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
-
 require_once '../system/autoload.inc.php';                                         //SYSTEM Classes
 require_once 'uVote/autoload.inc.php';                                           //Project Classes
-require_once '../system/log/register_exception_shortcut.php';                      //allow ERROR() instead of \SYSTEM\LOG\ERROR()
-require_once '../system/log/register_errorhandler_jsonoutput.php';                 //print errors as json to caller
-require_once '../system/log/register_result_shortcut.php'; 
        
 require_once 'config.php';
 SYSTEM\system::start($uvote_config);
+\SYSTEM\system::include_ExceptionShortcut();
+\SYSTEM\system::include_ResultShortcut();
+\SYSTEM\system::register_errorhandler_dbwriter();
+\SYSTEM\system::register_errorhandler_jsonoutput();
 
 echo \SYSTEM\API\api::run('\SYSTEM\API\verify','page_uvote',array_merge($_POST,$_GET),1,false,true)->html();
